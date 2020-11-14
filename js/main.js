43469c7efcec851f631518524c1476907f0017e9
KnifeSGS <knife.sgs@gmail.com>
2020-11-14 22:53:08 +0100

    modal fadeout nw

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
 'use strict';
 
-// Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal__close")[0];
+const span = document.getElementsByClassName("modal__close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+btn.onclick = () => {modal.style.display = "flex"; modal.classList.remove('modal--hide');};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = () => {modal.style.display = "none"; modal.classList.add('modal--hide');}
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    modal.classList.add('modal--hide');
   }
 }
